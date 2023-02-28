586bf01636778333edbbad1b52d4a618b3d8ff80
namanthanki <stuniz7855@gmail.com>
2023-03-01 00:03:30 +0530

    added icons

--- app/src/views/components/SideBar.js
+++ app/src/views/components/SideBar.js
@@ -1,4 +1,4 @@
-import { FaHome, FaUser } from 'react-icons/fa';
+import { FaHome, FaUser, FaCoins } from 'react-icons/fa';
 import { MdSpaceDashboard } from 'react-icons/md'
 
 const SideBar = () => {
@@ -6,6 +6,7 @@ const SideBar = () => {
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
             <SideBarIcon icon={<FaHome size='28' />} text='Home' />
             <SideBarIcon icon={<MdSpaceDashboard size='28' />} text='Dashboard' />
+            <SideBarIcon icon={<FaCoins size='28' />} text='Tokens' />
             <SideBarIcon icon={<FaUser size='28' />} text='Profile' />
         </div>
     );
