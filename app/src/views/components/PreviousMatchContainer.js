7f6cbe6a9f2ec172479833711d6b47e0fe8ff20a
namanthanki <stuniz7855@gmail.com>
2023-03-13 22:18:34 +0530

    fixed minor glitches

--- app/src/views/components/PreviousMatchContainer.js
+++ app/src/views/components/PreviousMatchContainer.js
@@ -8,9 +8,9 @@ class PreviousMatchContainer extends React.Component {
 
     render() {
     return (
+        <Link to={`/dashboard/view/${this.props.addr}`}>
         <div className="flex justify-between items-center w-80 bg-white p-5 rounded-lg">
-            <Link to={`/dashboard/view/${this.props.addr}`}>
-            <div>
+            {/* <div> */}
             <div className="">
                 <div className="rounded-full w-20 h-20 mb-1 bg-gray-600">
                     
@@ -20,15 +20,17 @@ class PreviousMatchContainer extends React.Component {
                     <span>{this.props.sideA}</span>
                 </div>
             </div>
+{/*             
             <div className='flex justify-evenly items-center text-3xl'>
                 <div> 
                     <p className='text-center my-2'>VS</p>
                 </div>
-            </div>
+            </div> */}
             <div className='flex flex-col justify-end gap-3 items-center text-3xl h-full'>
                 <div> 
                     <p className='text-lg mr-2 mb-1 text-center text-red-500'></p>
                     <p className='text-xs text-center text-black'>{dayjs.unix(this.props.date).format('DD/MM/YYYY')}</p>
+                    <p className='text-center my-2'>VS</p>
                 </div>
             </div>
             <div className="">
@@ -40,9 +42,9 @@ class PreviousMatchContainer extends React.Component {
                     <span>{this.props.sideB}</span>
                 </div>
             </div>
-            </div>
-            </Link>
+            {/* </div> */}
         </div>
+            </Link>
         )
     }
 }
